ce573626f464c8f2efef5967d223fe283fca0e20
Seth Yates <29878+sethyates@users.noreply.github.com>
2022-08-25 20:36:58 -0700

    fix: tool lookup

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,11 @@
 import * as core from '@actions/core'
 import * as exec from '@actions/exec'
-import * as tc from '@actions/tool-cache'
-import os from 'os'
 import {ExecOptions} from '@actions/exec/lib/interfaces'
 
 async function run(): Promise<void> {
   try {
     const versionSpec = core.getInput('version') || 'latest'
     core.debug(`version ${versionSpec}`)
-
-    const winch = tc.find('winch', versionSpec, os.arch())
-    core.debug(`found winch at ${winch}`)
 
     const command = core.getInput('command') || 'ci'
     core.debug(`command = ${command}`)
@@ -29,7 +24,7 @@ async function run(): Promise<void> {
       }
     }
 
-    await exec.exec(winch, ['--file', file, command], options)
+    await exec.exec('winch', ['--file', file, command], options)
   } catch (error) {
     if (error instanceof Error) core.setFailed(error.message)
   }
